10ce77e53ffa30773dffdc7593e02a1d49be4d3d
NekoPunch! <549643063@qq.com>
2026-07-05 20:45:42 +0800

    不是很懂点按Ctrl在干嘛

--- AquaMai.Mods/Utils/AntiLag.cs
+++ AquaMai.Mods/Utils/AntiLag.cs
@@ -6,51 +6,48 @@ using Main;
 using MelonLoader;
 using UnityEngine;
 
-namespace AquaMai.Mods.Utils;
-
-[ConfigSection(
-    name: "狂暴引擎",
-    en: "Some tricks to prevent the system from lagging",
-    zh: "可能缓解掉帧，但也可能把狂暴转移到用户身上")]
-public class AntiLag : MonoBehaviour
+namespace AquaMai.Mods.Utils
 {
-    [ConfigEntry(name: "后台运行", zh: "游戏未取得焦点时也运行")]
-    private static readonly bool activateWhileBackground = false;
-
-    [ConfigEntry(name: "高优先级", zh: "将游戏设为高优先级")]
-    private static readonly bool setHighPriority = true;
-
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(GameMainObject), "Awake")]
-    public static void OnGameMainObjectAwake()
+    [ConfigSection("狂暴引擎",
+                      "Some tricks to prevent the system from lagging",
+                      "可能缓解掉帧，但也可能把狂暴转移到用户身上")]
+    public class AntiLag : MonoBehaviour
     {
-        var go = new GameObject("妙妙防掉帧");
-        go.AddComponent<AntiLag>();
-        if (setHighPriority)
+        [ConfigEntry("后台运行", zh: "游戏未取得焦点时也运行")]
+        private static readonly bool activateWhileBackground = false;
+
+        [ConfigEntry("高优先级", zh: "将游戏设为高优先级")] private static readonly bool setHighPriority = true;
+
+        [HarmonyPostfix, HarmonyPatch(typeof(GameMainObject), "Awake")]
+        public static void OnGameMainObjectAwake()
         {
-            System.Diagnostics.Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+            var go = new GameObject("妙妙防掉帧");
+            go.AddComponent<AntiLag>();
+            if (setHighPriority)
+            {
+                System.Diagnostics.Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High;
+            }
         }
-    }
 
-    private void Awake()
-    {
-        InvokeRepeating(nameof(OnTimer), 10f, 10f);
-    }
+        private void Awake() => InvokeRepeating(nameof(OnTimer), 10f, 10f);
 
-    [DllImport("user32.dll", EntryPoint = "keybd_event", SetLastError = true)]
-    private static extern void keybd_event(uint bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+        [DllImport("user32.dll", EntryPoint = "keybd_event", SetLastError = true)]
+        private static extern void keybd_event(uint bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
 
-    const int KEYEVENTF_KEYDOWN = 0x0000;
-    const int KEYEVENTF_KEYUP = 0x0002;
-    const int CTRL = 17;
+        private const int KEYEVENTF_KEYDOWN = 0x0000;
+        private const int KEYEVENTF_KEYUP = 0x0002;
+        private const int VK_CONTROL = 17;
 
-    private void OnTimer()
-    {
-        if (!Application.isFocused && !activateWhileBackground) return;
+        private void OnTimer()
+        {
 #if DEBUG
-        MelonLogger.Msg("[AntiLag] Trigger");
+            MelonLogger.Msg("[AntiLag] Trigger");
 #endif
-        keybd_event(CTRL, 0, KEYEVENTF_KEYDOWN, 0);
-        keybd_event(CTRL, 0, KEYEVENTF_KEYUP, 0);
+            if (Application.isFocused || activateWhileBackground)
+            {
+                keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYDOWN, 0);
+                keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);
+            }
+        }
     }
 }
